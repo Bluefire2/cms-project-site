6f2197e441c32f66eb569b942ef19a7553293295
Kyrylo Chernyshov <kc875@cornell.edu>
2018-10-17 00:00:15 -0400

    Add a few people

--- src/constants.js
+++ src/constants.js
@@ -1,18 +1,30 @@
 import portraitPlaceholder from "./img/portrait-placeholder.png";
 
 export const teams = [{
-    name: "frontend",
-    subheading: "\"If your website doesn't look good, how do you expect to look good?\" --Bill Gates, 1997",
+    name: "core team",
+    subheading: "\"It's pronounced see-kwell.\" --Anonymous",
     members: [
         {
-            name: "John Doe",
+            name: "Andrew Myers",
+            role: "Project administrator",
+            portrait: portraitPlaceholder,
+            github: "https://github.com/andrewcmyers",
+            website: "https://www.cs.cornell.edu/andru/"
+        },
+        {
+            name: "Michael Stella",
             role: "Team Lead",
             portrait: portraitPlaceholder,
-            github: "https://github.coecis.cornell.edu/CMSX/cmsx",
-            linkedin: "https://www.linkedin.com/company/john-doe/"
+            github: "https://github.com/mpsrig",
+            linkedin: "https://www.linkedin.com/in/mpsrig"
         },
-        {name: "John Doe", role: "Team Lead", portrait: portraitPlaceholder},
-        {name: "John Doe", role: "Team Lead", portrait: portraitPlaceholder},
+        {
+            name: "Kyrylo Chernyshov",
+            role: "Team Member",
+            portrait: portraitPlaceholder,
+            github: "https://github.com/Bluefire2",
+            linkedin: "https://www.linkedin.com/in/kc875"
+        },
         {name: "John Doe", role: "Team Lead", portrait: portraitPlaceholder},
         {name: "John Doe", role: "Team Lead", portrait: portraitPlaceholder},
         {name: "John Doe", role: "Team Lead", portrait: portraitPlaceholder},
